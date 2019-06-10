ea4eda004c22cce216b3b8d11ba02ab1010ba5be
stevekutz <stkutz@gmail.com>
2019-06-09 23:52:28 -0500

    added findBearsById

--- bears/bears-model.js
+++ bears/bears-model.js
@@ -3,12 +3,19 @@ const dbStretch = require('../data/dbConfig');
 // These resolve to a promise
 module.exports = {
     findBears,
+    findBearsById,
     addBears,
 }
 
 function findBears() {
     return dbStretch('bears');
 }
+
+function findBearsById(id) {
+    return dbStretch('bears')
+        .where('id', id);
+}
+
 
 function addBears (newBear) {
     return dbStretch('bears')
--- bears/bears-router.js
+++ bears/bears-router.js
@@ -13,6 +13,25 @@ router.get('/', (req,res) => {
         })
 });
 
+// GET uses 'findBearsByID from bears-model.js
+router.get('/:id', (req, res) => {
+    const {id} = req.params;
+
+    Bears.findBearsById(id)
+        .then(bearName => {
+            if(bearName) {
+                res.status(200).json(bearName);
+            } else {
+                res.status(404).json({
+                    message: ` Bear with id ${id} not found`
+                })
+            }
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        })
+})
+
 
 
 // POST uses 'addBears' from bears-model.js
